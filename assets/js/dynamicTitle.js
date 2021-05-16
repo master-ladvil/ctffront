5c7e0bceb68a53c7302888780d6dc2b019b3fe7f
master-ladvil <srihariharanb21@gmail.com>
2021-05-16 16:46:01 +0530

    final push 2

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -35,41 +35,4 @@ window.onload = function () {
       }
     }, 1000);
   }
-
-
-window.onload = function () {
-  const favicon = document.getElementById("favicon");
-  let pageTitle = document.title;
-  let attentionMessage = "Come back";
-  let blinkEvent = null;
-
-  document.addEventListener("visibilitychange", function (e) {
-    let isPageActive = !document.hidden;
-
-    if (!isPageActive) {
-      blink();
-    } else {
-      document.title = pageTitle;
-      clearInterval(blinkEvent);
-    }
-  });
-
-  document.addEventListener("visibilitychange", function (e) {
-    if (!document.hidden) {
-      document.title = pageTitle;
-      favicon.href = "./assets/images/dp_male.svg";
-    }
-  });
-
-  function blink() {
-    blinkEvent = setInterval(() => {
-      if (document.title === attentionMessage) {
-        document.title = pageTitle;
-        favicon.href = "./assets/images/dp_male.svg";
-      } else {
-        document.title = attentionMessage;
-        favicon.href = "./assets/images/folded.png";
-      }
-    }, 1000);
-  }
 };
